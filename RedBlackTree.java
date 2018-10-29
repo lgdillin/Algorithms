c2e21cfd8409d69416a996ee7a8aa6ba1582c7cc
lgdillin <mailgd9@gmail.com>
2018-10-28 21:40:06 -0500

    started delete and delete fixup (Insert still broken)

--- RedBlackTree.java
+++ RedBlackTree.java
@@ -34,6 +34,12 @@ class RedBlackTree extends BinarySearchTree {
     root = nil;
   }
 
+  static RBNode rbMinimum(RBNode x) {
+    while(x.left != null)
+      x = x.left;
+    return x;
+  }
+
   static void leftRotate(RedBlackTree rbt, RBNode x) {
     RBNode y = x.right; // set y
     x.right = y.left; // turn y's left subtree into x's right subtree
@@ -74,18 +80,9 @@ class RedBlackTree extends BinarySearchTree {
     y.parent = x;
   }
 
-  void print(RBNode x) {
-    if(x != nil) {
-      print(x.left);
-      System.out.println(x.key);
-      print(x.right);
-    }
-  }
-
   static void rbInsert(RedBlackTree rbt, RBNode z) {
     RBNode y = rbt.nil;
     RBNode x = rbt.root;
-
 
     while(x != rbt.nil) {
       y = x;
@@ -166,15 +163,152 @@ class RedBlackTree extends BinarySearchTree {
     rbt.root.color = Color.BLACK;
   }
 
+  static void rbDelete(RedBlackTree rbt, RBNode z) {
+    RBNode y = z;
+    Color yOriginalColor = y.color;
+
+    RBNode x;
+    if(z.left == rbt.nil) {
+      x = z.right;
+      transplant(rbt, z, z.right);
+    } else if(z.right == rbt.nil){
+      x = z.left;
+      transplant(rbt, z, z.left);
+    } else {
+      y = rbMinimum(z.right);
+      yOriginalColor = y.color;
+      x = y.right;
+
+      if(y.parent == z) {
+        x.parent = y;
+      } else {
+        transplant(rbt, y, y.right);
+        y.right = z.right;
+        y.right.parent = y;
+      }
+
+      transplant(rbt, z, y);
+      y.left = z.left;
+      y.left.parent = y;
+      y.color = z.color;
+    }
+
+    if(yOriginalColor == Color.BLACK)
+      rbDeleteFix(rbt, x);
+  }
+
+  static void rbDeleteFix(RedBlackTree rbt, RBNode x) {
+    while(x != rbt.root && x.color != Color.BLACK) {
+      if(x == x.parent.left) {
+        RBNode w = x.parent.left;
+
+        // Case 1: z's uncle is red (y is red)
+        if(w.color == Color.RED) {
+          w.color = Color.BLACK;
+          x.parent.color = Color.RED;
+          leftRotate(rbt, x.parent);
+          w = x.parent.right;
+        }
+
+        // Case 2: if y is black and z is a right child
+        if(w.left.color == Color.BLACK && w.right.color == Color.BLACK) {
+          w.color = Color.RED;
+          x = x.parent;
+
+        // Case 3: y is black and z is a right child
+        } else if(w.right.color == Color.BLACK) {
+          w.left.color = Color.BLACK;
+          w.color = Color.RED;
+          rightRotate(rbt, w);
+          w = x.parent.right;
+        } else {
+          // Case 4: x’s sibling w is black, and w’s right child is red
+          w.color = x.parent.color;
+          x.parent.color = Color.BLACK;
+          w.right.color = Color.BLACK;
+          leftRotate(rbt, x.parent);
+          x = rbt.root;
+        }
+
+      } else {
+        RBNode w = x.parent.right;
+
+        // Case 1: z's uncle is red (w is red)
+        if(w.color == Color.RED) {
+          w.color = Color.BLACK;
+          x.parent.color = Color.RED;
+          rightRotate(rbt, x.parent);
+          w = x.parent.left;
+        }
+
+        // Case 2: if w is black and z is a left child
+        if(w.right.color == Color.BLACK && w.left.color == Color.BLACK) {
+          w.color = Color.RED;
+          x = x.parent;
+
+        // Case 3: x’s sibling w is black, w’s left child is red, and w’s right child is black
+        } else if(w.left.color == Color.BLACK) {
+            w.right.color = Color.BLACK;
+            w.color = Color.RED;
+            leftRotate(rbt, w);
+            w = x.parent.left;
+        } else {
+          // Case 4: x’s sibling w is black, and w’s right child is red
+          w.color = x.parent.color;
+          x.parent.color = Color.BLACK;
+          w.right.color = Color.BLACK;
+          leftRotate(rbt, x.parent);
+          x = rbt.root;
+        }
+
+      }
+    }
+  }
+
+  static void transplant(RedBlackTree rbt, RBNode u, RBNode v) {
+    if(u.parent == rbt.nil) {
+      rbt.root = v;
+    } else if(u == u.parent.left) {
+      u.parent.left = v;
+    } else {
+      u.parent.right = v;
+      v.parent = u.parent;
+    }
+  }
+
+  void print(RBNode x) {
+    if(x != nil) {
+      print(x.left);
+      System.out.println(x.key);
+      print(x.right);
+    }
+  }
+
   public static void main(String[] args) {
     Random r = new Random(123456);
     RedBlackTree rbt = new RedBlackTree();
 
+    // Delete these nodes;
+    RBNode a = null, b = null, c = null;
+
     for(int i = 0; i < 30; ++i) {
       RBNode x = new RBNode(r.nextInt(300));
       RedBlackTree.rbInsert(rbt, x);
+
+      // if(r.nextDouble() < 0.2)
+      //   a = x;
+      // else if(r.nextDouble() < 0.5)
+      //   b = x;
+      // else if(r.nextDouble() < 0.7)
+      //   c = x;
     }
 
     rbt.print(rbt.root);
+
+    if(a != null) RedBlackTree.rbDelete(rbt, a);
+    if(b != null) RedBlackTree.rbDelete(rbt, b);
+    if(c != null) RedBlackTree.rbDelete(rbt, c);
+
+    //rbt.print(rbt.root);
   }
 }
